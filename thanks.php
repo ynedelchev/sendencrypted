6f3941ef8f764fb7fda34a17e670cf487f973af1
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2020-04-20 01:17:01 +0300

    Better translation to Bulgarian

--- thanks.php
+++ thanks.php
@@ -5,7 +5,7 @@ if (!isset($uplevels)) {
 ?>
     <div style="font-size: 0.75em; float: right;">Благодарности на:
        <a href="https://digitalbazaar.com/forge/" target="_blank">
-          <img class="thanks" src="<?php echo full_url($uplevels);?>/images/db.png" alt="Дигитал Базар - Фордж (Джаваскрипт библиотека за криптиране)" title="Дигитал Базар - Фордж (Джаваскрипт библиотека за криптиране)"/>       </a>
+          <img class="thanks" src="<?php echo full_url($uplevels);?>/images/db.png" alt="Диджитал Базар - Фордж (Джаваскрипт библиотека за криптиране)" title="Диджитал Базар - Фордж (Джаваскрипт библиотека за криптиране)"/>       </a>
        <a href="http://gildas-lormeau.github.io/zip.js/" target="_blank">
           <img class="thanks" src="<?php echo full_url($uplevels);?>/images/zip.png" alt="Роб В. - Зип (Джаваскрипт библиотека)"  title="Роб В. - Зип (Джаваскрипт библиотека)"/>
        </a>
@@ -13,7 +13,7 @@ if (!isset($uplevels)) {
           <img class="thanks" src="<?php echo full_url($uplevels);?>/images/vim.png" alt="Вим (Текстов редактор)"  title="Вим (Текстов редактор)"/>
        </a>
        <a href="https://www.html5rocks.com/en/tutorials/file/dndfiles/" target="_blank">
-          <img class="thanks" src="<?php echo full_url($uplevels);?>/images/html5.png" alt="Блог на Ерик Бидерман" title="Блог на Ерик Бидерман"/>
+          <img class="thanks" src="<?php echo full_url($uplevels);?>/images/html5.png" alt="Рубрика на Ерик Бидерман" title="Рубрика на Ерик Бидерман"/>
        </a>
        <a href="https://send.firefox.com/" target="_blank">
           <img class="thanks" src="<?php echo full_url($uplevels);?>/images/send.svg" alt="Изпращане на криптирани файлове през аналогична услуга на Файърфокс"  title="Изпращане на криптирани файлове през аналогична услуга на Файърфокс"/>
