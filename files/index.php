96e937fc791fd8f2648b5b3333672abc740a6d18
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2020-04-20 01:59:29 +0300

    Replace __DIR__ wiht $dir that is initially initialized with __DIR__. Also remove var_dump(...)

--- files/index.php
+++ files/index.php
@@ -113,7 +113,6 @@ function del() {
   getenv('HTTP_FORWARDED')?:
   getenv('REMOTE_ADDR');
   if ($ip != 'localhost' && $ip != '127.0.0.1' && $ip != '::1') {
-var_dump($ip);
     http_response_code(404);
     error_log('Trying to access '.__FILE__.' from IP '.$ip.'. That is not allowed. Only calls from localhost or 127.0.0.1 are allowed.');
     exit(1);
@@ -129,18 +128,19 @@ var_dump($ip);
   }
   $deleted = array();
   $failed  = array(); 
-  if ($dh = opendir(__DIR__)) {
+  $dir = __DIR__;
+  if ($dh = opendir($dir)) {
     while (($file = readdir($dh)) !== FALSE) {
-      if (is_dir(__DIR__.'/'.$file)) {
+      if (is_dir($dir.'/'.$file)) {
         continue;
       }
       if (substr($file, -strlen(".json")) != ".json") {
         continue;
       }
       $id = substr($file, 0, strlen($file)-5);
-      $content = file_get_contents(__DIR__.'/'.$file);
+      $content = file_get_contents($dir.'/'.$file);
       if ($content === FALSE) {
-        error_log("Content of file \"".__DIR__.'/'.$file."\" cannot be read. Thus not deleting it as part of delete expired files operation.");
+        error_log("Content of file \"".$dir.'/'.$file."\" cannot be read. Thus not deleting it as part of delete expired files operation.");
         $failed[$id] = "Cannot read metadata.";
         continue;
       }
@@ -149,17 +149,17 @@ var_dump($ip);
       $meta = json_decode($content, $objects_to_associative_arrays, $recursion_limit, JSON_BIGINT_AS_STRING);
       if ($meta === NULL) {
         $content = strlen($content) > 1024 ? "Content starts with : \"".substr($content, 0, 1024)."\" ..." : "Content is: \"".$content."\".";
-        error_log("Content of file \"".__DIR__.'/'.$file."\" cannot be JSON decoded or the encoded data is deeper than the recursion limit ($recursion_limit). Thus not deleting it as part of delete expired files operation.");
+        error_log("Content of file \"".$dir.'/'.$file."\" cannot be JSON decoded or the encoded data is deeper than the recursion limit ($recursion_limit). Thus not deleting it as part of delete expired files operation.");
         $failed[$id] = "Cannot parse metadata.";
         continue;
       } 
       if (!isset($meta['status'])) {
-        error_log("Metadata file \"".__DIR__.'/'.$file."\" does not specify \"status\" property, thus it is unknown what the status of the corresponding data file is. Not deleting it as part of delete expired files operation. Content of metadata file is: ".json_encode($meta));
+        error_log("Metadata file \"".$dir.'/'.$file."\" does not specify \"status\" property, thus it is unknown what the status of the corresponding data file is. Not deleting it as part of delete expired files operation. Content of metadata file is: ".json_encode($meta));
         $failed[$id] = "Cannot find status.";
         continue;
       }
       if (!isset($meta['expires'])) {
-        error_log("Metadata file \"".__DIR__.'/'.$file."\" does not specify \"expires\" property, thus it is unknown when the corresponding data file expires. Not deleting it as part of delete expired files operation. Content of metadata file is: ".json_encode($meta));
+        error_log("Metadata file \"".$dir.'/'.$file."\" does not specify \"expires\" property, thus it is unknown when the corresponding data file expires. Not deleting it as part of delete expired files operation. Content of metadata file is: ".json_encode($meta));
         $failed[$id] = "Cannot find expiration date.";
         continue;
       }
@@ -167,7 +167,7 @@ var_dump($ip);
         continue;
       }
       if ($meta['status'] == "unknown") {
-        error_log("Status defined in metadata file \"".__DIR__.'/'.$file."\" is \"".$meta["status"]."\", so it would not be deleted.");
+        error_log("Status defined in metadata file \"".$dir.'/'.$file."\" is \"".$meta["status"]."\", so it would not be deleted.");
         $failed[$id] = "Unknown status.";
         continue;
       }
@@ -178,25 +178,25 @@ var_dump($ip);
         $now  = date("Y-m-d H:i s (P)", $now);
         $expired  = date("Y-m-d H:i s (P)", $expires);
         $data = substr($file, 0, strlen($file)-5).".data";
-        $msgmeta = "Deleting \"".__DIR__.'/'.$file."\" expired on ".$expired.". Now : ".$now.".";
-        $msgdata = "Deleting \"".__DIR__.'/'.$data."\" expired on ".$expired.". Now : ".$now.".";
+        $msgmeta = "Deleting \"".$dir.'/'.$file."\" expired on ".$expired.". Now : ".$now.".";
+        $msgdata = "Deleting \"".$dir.'/'.$data."\" expired on ".$expired.". Now : ".$now.".";
         $successdata = FALSE;
         $successmeta = FALSE;
         error_log($msgdata);
-        if (file_exists(__DIR__.'/'.$data)) {
-          $successdata = unlink(__DIR__.'/'.$data);
+        if (file_exists($dir.'/'.$data)) {
+          $successdata = unlink($dir.'/'.$data);
           if ($successdata === FALSE) {
-            error_log("Failed   \"".__DIR__.'/'.$data."\" deletion.");
+            error_log("Failed   \"".$dir.'/'.$data."\" deletion.");
             $failed[$id] = "Data deletion failure.";
           }
         } else {
           $successdata = TRUE;
         }
         error_log($msgmeta);
-        if (file_exists(__DIR__.'/'.$file)) {
-          $successmeta = unlink(__DIR__.'/'.$file);
+        if (file_exists($dir.'/'.$file)) {
+          $successmeta = unlink($dir.'/'.$file);
           if ($successmeta === FALSE) {
-            error_log("Failed   \"".__DIR__.'/'.$file."\" deletion.");
+            error_log("Failed   \"".$dir.'/'.$file."\" deletion.");
             $failed[$id] = "Metadata deletion failure.";
           }
         } else {
@@ -211,7 +211,7 @@ var_dump($ip);
     $obj = array("deleted"=>$deleted, "failed"=>$failed);
     echo json_encode($obj, $json_encode_props)."\n";
   } else {
-    http_exit(500, "Вътрешносървърна грешка", "Internal Server Error. Cannot open directory \"".__DIR__."\" for reading. Cannot process data and metadata files in this directory.", null);
+    http_exit(500, "Вътрешносървърна грешка", "Internal Server Error. Cannot open directory \"".$dir."\" for reading. Cannot process data and metadata files in this directory.", null);
   }
 }   
 
